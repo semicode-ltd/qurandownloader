8b3c18ef3038258a6327419b39ef2aa842380380
SemiCode Ltd <semicode777@gmail.com>
2015-04-27 12:24:45 +0300

    Update qurandl.sh

--- qurandl.sh
+++ qurandl.sh
@@ -1,4 +1,10 @@
-for i in `seq -w 1 114` ; 
-do 
-wget "http://download.quranicaudio.com/quran/abdul_basit_murattal/$i.mp3" ; 
-done
+# This Script Will Download Entire Quran By Abdul basit Murattal Audition in mp3 Format 
+# This Script Licensed Under GPL V2 So You Should Recive A copy of GPL with it :)
+# Author : Mohamed Saif Eldeen 
+# Company : SemiCode Inc.
+for i in `seq -w 1 114` ; # 114 Sora in Quran 
+do  # Start To Looping :) 
+wget "http://download.quranicaudio.com/quran/abdul_basit_murattal/$i.mp3" ;  # Download Link 
+done # End Of Loop
+
+# Feel Very To Edit It As You Like ;)
